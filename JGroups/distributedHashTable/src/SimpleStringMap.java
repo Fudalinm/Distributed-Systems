5ec723b03916a0b0b26885cea65b910cfeafb3a0
Marek Fudaliński <fudalinm@gmail.com>
2019-03-25 21:29:00 +0100

    Removed unneccessery directory

--- JGroups/distributedHashTable/src/SimpleStringMap.java
+++
@@ -1,9 +0,0 @@
-public interface SimpleStringMap {
-    boolean containsKey(String key);
-
-    Integer get(String key);
-
-    void put(String key, Integer value);
-
-    Integer remove(String key);
-}
